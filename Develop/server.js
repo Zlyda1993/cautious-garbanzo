8860bfe8987c2e9575b87ec3c86bd5570cde6e3d
Zachary Lyda <Oth1zlyda1993@gmail.com>
2023-10-08 23:52:32 -0400

    Filled out the readme.

--- Develop/server.js
+++ Develop/server.js
@@ -1,72 +1,100 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 const PORT = 3001;
 
 const app = express();
 
-// middleware
+
 app.use(express.static("public"))
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-// routes
-app.get("/notes", (req, res) => {
-    // res.send("hi!")
 
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 })
 
-// app.get("/assets/css/styles.css", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./public/assets/css/styles.css"))
-// })
+app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf-8", function (err, data) {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: "An error occurred while reading the database." });
+            return;
+        }
 
-app.get("/", (req, res) => {
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
+})
+
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"))
 })
 
-
-app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./db/db.json"))
-})
-
 app.post("/api/notes", (req, res) => {
-
     const newNote = req.body;
 
-    console.log(newNote);
+    newNote.id = uuidv4();
 
     fs.readFile("./db/db.json", "utf-8", function (err, data) {
-        console.log(data)
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: "An error occurred while reading the database." });
+            return;
+        }
 
-        const notes = JSON.parse(data)
-
+        const notes = JSON.parse(data);
         notes.push(newNote);
-
-        console.log(notes)
 
         fs.writeFile("./db/db.json", JSON.stringify(notes, null, 4), function (err) {
             if (err) {
-                console.log(err);
+                console.error(err);
+                res.status(500).json({ error: "An error occurred while writing to the database." });
                 return;
             }
 
-
-            res.send("Successful!")
-        })
-
-    })
-
-
+            res.json(newNote);
+        });
+    });
 })
 
+app.delete("/api/notes/:id", (req, res) => {
+    const noteId = req.params.id;
 
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: "An error occurred while reading the database." });
+            return;
+        }
+
+        const notes = JSON.parse(data);
+
+        const index = notes.findIndex(note => note.id === noteId);
+
+        if (index === -1) {
+
+            res.status(404).json({ error: "Note not found." });
+        } else {
+
+            notes.splice(index, 1);
+
+            fs.writeFile("./db/db.json", JSON.stringify(notes, null, 4), err => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).json({ error: "An error occurred while writing to the database." });
+                } else {
+                    res.json({ message: "Note deleted successfully." });
+                }
+            });
+        }
+    });
+});
 
 app.listen(PORT, () => {
     console.log("Server is now running!")
 })
-
-
-
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # cautious-garbanzo
+
+# Steps
+
+## First I need to link express library and the fs library.
+
+## I created a port for the local.
+
+## I created a const for allowing access to the express library.
+
+## using a const of v4: uuidv4 and requiring the library of uuid i can later make unique ids for each array item made.
+
+## I had to add a const of path to require path so i could join the certain path to the dirname which allows the application to work on other computers since their file structure will by different than mine.
+
+## using middleware I was able to make paths for everything within the public folder so that there aren't crazy amounts of code just to get each of the static files to run properly.
+
+## The first get function connects the endpoint of /notes to the dirname which is all the way to this file and connects the path to the notes.html so that when /notes is added on to the original http it'll load my notes.html page.
+
+## The second get function connects the endpoint of /api/notes to read the file of db.json which is the array with all of our data. It parses the array to change them back to objects within the array.
+
+## the third get function connects the "*" symbol to the index.html. What this is doing is saying that any path added onto the original http address will got to the index.html and display it unless otherwise specified like /api/notes was done earlier.
+
+## The post method is now taking the data that the user sent in and writing it the db.json file which holds our array of data and also giving each saved object to be given an unique id number so that is can be called upon later.
+
+## The delete method is allowing the user to remove any saved database data based on its unique id number that was assigned earlier. The id number is targeted within the index.js file inder the delete method, saying when the delete button is pressed then target this id and remove it.
+
+# Special Thanks
+
+## A special thanks to Joem Casusi (Tutor) for helping walk me through this. His explanation of how the get, post, and delete helped me understand it way more and his example and practice method helped me to understand how to write these functions and he also taught me more on what the middleware does and how to use the devtools to further understand why my application isn't working.
